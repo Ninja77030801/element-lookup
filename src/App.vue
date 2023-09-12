7d8a9e63baec30e81dc7217b91f80daf6e187365
Ninja77030 <n77030@outlook.com>
2023-09-12 23:02:49 +0530

    Added UI and made modifications

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <h1>Element Lookup</h1>
-  Search by: <select v-model="lookupMethod">
+  <h1 style="text-align: center; margin-top: 56px;" class="title">Element Lookup</h1>
+  <p style="text-align: center;">Search by: <select v-model="lookupMethod" class="select">
     <option selected value="name">Name</option>
     <option value="symbol">Symbol</option>
     <option value="number">Number</option>
-  </select><br />
-  <input v-if="lookupMethod == 'name'" type="text" placeholder="Search by name" v-model="wantedElementInfo" @keyup.enter="LookupElement">
-  <input v-if="lookupMethod == 'symbol'" placeholder="Search by symbol" v-model="wantedElementInfo" @keyup.enter="LookupElement" type="text">
-  <input v-if="lookupMethod == 'number'" placeholder="Search by atomic number" v-model="wantedElementInfo" @keyup.enter="LookupElement" type="text">
-  <button @click="LookupElement">Search</button>
-  <div v-if="searchComplete">
-    <h1>{{ name }}</h1>
+  </select></p><br />
+  <div v-if="lookupMethod" style="text-align: center;">
+    <input style="margin-left: auto; margin-right: auto" v-if="lookupMethod == 'name'" class="input custom-width" type="text" placeholder="Search by name" v-model="wantedElementInfo" @keyup.enter="LookupElement">
+    <input style="margin-left: auto; margin-right: auto" v-if="lookupMethod == 'symbol'" class="input custom-width" placeholder="Search by symbol" v-model="wantedElementInfo" @keyup.enter="LookupElement" type="text">
+    <input style="margin-left: auto; margin-right: auto" v-if="lookupMethod == 'number'" class="input custom-width" placeholder="Search by atomic number" v-model="wantedElementInfo" @keyup.enter="LookupElement" type="text">
+    <button style="margin-right: auto;" class="button is-primary" @click="LookupElement">Search</button>
+  </div>
+  <div style="margin-top: 30px;" v-if="searchComplete" class="el-info">
+    <h1 class="title">{{ name }}</h1>
     Symbol: {{ symbol }}<br />
     Atomic number: {{ number }}<br />
     Atomic mass number: {{ mass }}<br />
     Appearance: {{ appearance || "Not given" }}<br />
     Discovered by {{ discoveredBy }}
+  </div>
+  <div style="margin-top: 40%;">
+    <a style="margin-left: 80%" href="https://ninja77030801.netlify.app" target="_blank"><button class="button is-link">About the developer</button></a>
   </div>
 </template>
 <script>
@@ -32,42 +37,47 @@ export default {
       mass: "",
       symbol: "",
       discoveredBy: "",
-      searchComplete: false
+      searchComplete: false,
     }
   },
   methods: {
     LookupElement(){
-      var wantedElement
-      switch(this.lookupMethod){
-        case 'name':
-          wantedElement = npt.getByName(this.wantedElementInfo)
-          break
-        case 'symbol':
-          wantedElement = npt.getBySymbol(this.wantedElementInfo)
-          break
-        case 'number':
-          wantedElement = npt.getByNumber(this.wantedElementInfo)
-          break
+      try {
+        var wantedElement
+        switch(this.lookupMethod){
+          case 'name':
+            wantedElement = npt.getByName(this.wantedElementInfo)
+            break
+          case 'symbol':
+            wantedElement = npt.getBySymbol(this.wantedElementInfo)
+            break
+          case 'number':
+            wantedElement = npt.getByNumber(this.wantedElementInfo)
+            break
+        }
+        this.name = wantedElement.name
+        this.number = wantedElement.number
+        this.appearance = wantedElement.appearance
+        this.mass = wantedElement.atomic_mass
+        this.symbol = wantedElement.symbol
+        this.discoveredBy = wantedElement.discovered_by
+        this.searchComplete = true
       }
-      this.name = wantedElement.name
-      this.number = wantedElement.number
-      this.appearance = wantedElement.appearance
-      this.mass = wantedElement.atomic_mass
-      this.symbol = wantedElement.symbol
-      this.discoveredBy = wantedElement.discovered_by
-      this.searchComplete = true
+      catch(error){
+        alert(error)
+      }
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.custom-width {
+  width: 50%;
+}
+
+.el-info {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  justify-content: center;
 }
 </style>
